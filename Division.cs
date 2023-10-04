f0960c4674c5c6a693111679916a9ba75f7f81b0
glabojko <glabojko@gmail.com>
2023-10-04 21:07:24 +0200

    methods foradding instances to division added

--- Division.cs
+++ Division.cs
@@ -8,5 +8,47 @@ namespace Panzerfaust
 {
     public class Division
     {
+        public string Name { get; set; }
+        public List<Tank> tanks;
+        public List<Truck> trucks;
+        public List<Panzer> panzers;
+        public List<Gunner> gunners;
+        public List<Infantry> infantrys;
+
+        public Division(string name)
+        {
+            tanks = new List<Tank>();
+            trucks = new List<Truck>();
+            panzers = new List<Panzer>();
+            gunners = new List<Gunner>();
+            infantrys = new List<Infantry>();
+            Name = name;
+        }
+
+
+        public void AddTank(Tank tank)
+        {
+            tanks.Add(tank);
+        }
+
+        public void AddTruck(Truck truck)
+        {
+            trucks.Add(truck);
+        }
+
+        public void AddPanzer(Panzer panzer)
+        {
+            panzers.Add(panzer);
+        }
+
+        public void AddGunner(Gunner gunner)
+        {
+            gunners.Add(gunner);
+        }
+
+        public void AddInfantry(Infantry infantry)
+        {
+            infantrys.Add(infantry);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,8 +6,24 @@
         {
             Console.WriteLine("Herzlich wilkommen im unsere Panzerfaust!");
 
-            Dictionary<string, Division> leibstandarteAdolfHitler = new Dictionary<string, Division>();
-            Dictionary<string, Division> dasReich = new Dictionary<string, Division>();
+            //Dictionary<string, Division> leibstandarteAdolfHitler = new Dictionary<string, Division>();
+            //Dictionary<string, Division> dasReich = new Dictionary<string, Division>();
+
+            var tank1 = new Tank(1, 40, 1500);
+            var truck1 = new Truck(2, 80);
+            var infantry1 = new Infantry(1);
+            var gunner1 = new Gunner(1);
+            var panzer1 = new Panzer(1);
+
+            //więcej czołgów for
+
+            var ah = new Division("leibstandarteAdolfHitler");
+
+            ah.AddTank(tank1);
+            ah.AddTruck(truck1);
+            ah.AddInfantry(infantry1);
+            ah.AddGunner(gunner1);
+            ah.AddPanzer(panzer1);
         }
     }
 }
